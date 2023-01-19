bef153c21c7967cdbd76bde688922ffd9aa1ea02
Nadia Halkina <nadia.esperanza.2000@gmail.com>
2023-01-19 15:01:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let pos = Math.abs(n);
+    let str = pos.toString();
+    let arr = Number(str.split('').reverse().join(''));
+    
+    return arr;    
 }
